dd5340f323bb6952c3a8570fdf1cd638bb525bd9
Adrian Sampson <adrian@radbox.org>
2025-12-21 10:05:09 -0500

    More helpful listening URL log

--- src/serve.rs
+++ src/serve.rs
@@ -22,7 +22,7 @@ pub async fn serve(ctx: Context) {
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
-    eprintln!("listening on {}", listener.local_addr().unwrap());
+    eprintln!("listening on http://{}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
 
